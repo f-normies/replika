c2b3ac6825e6bb9c932bf51da713411e8ebb1c31
f-normies <dooggest@gmail.com>
2026-07-15 16:46:19 +0300

    chore: SwiftPM skeleton with speech-swift dependency

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+let package = Package(
+    name: "Replika",
+    platforms: [.macOS(.v15)],
+    dependencies: [
+        .package(url: "https://github.com/soniqo/speech-swift", branch: "main")
+    ],
+    targets: [
+        .target(name: "ReplikaCore"),
+        .target(
+            name: "SpeechSwiftProvider",
+            dependencies: [
+                "ReplikaCore",
+                .product(name: "Qwen3ASR", package: "speech-swift"),
+                .product(name: "SpeechVAD", package: "speech-swift")
+            ]
+        ),
+        .executableTarget(
+            name: "asr-probe",
+            dependencies: [.product(name: "Qwen3ASR", package: "speech-swift")]
+        )
+    ]
+)
---
+++ Sources/asr-probe/main.swift
@@ -0,0 +1,1 @@
+print("asr-probe: build ok")
